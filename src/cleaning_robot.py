1b12d128d110675beed20719d328da616f47b9d2
Sebastian W <117441091+Sebastiandotpy@users.noreply.github.com>
2023-07-05 15:55:05 +0200

    added solution

--- src/cleaning_robot.py
+++ src/cleaning_robot.py
@@ -3,45 +3,63 @@ from motor import run
 
 class CleaningRobot:
     def __init__(self, testing_mode=False):
+        """Initialize the CleaningRobot instance."""
         self.testing_mode = testing_mode
+        self.inner_door_unlocked = False
 
     def unlock_door(self):
+        """Unlock the door."""
         if not self.testing_mode:
             run("unlock_door")
+        self.inner_door_unlocked = True
         return "Door unlocked.\n"
 
     def lock_door(self):
+        """Lock the door."""
         if not self.testing_mode:
             run("lock_door")
+        self.inner_door_unlocked = False  # Set inner door unlocked status to False
         return "Door locked.\n"
 
     def enter_door(self):
+        """Enter the door."""
         if not self.testing_mode:
             run("enter_door")
-        return "Door entered.\n"
+        return ""  # Return an empty string
 
     def turn_around(self):
+        """Turn around."""
         if not self.testing_mode:
             run("turn_around")
         return "Turn around.\n"
 
     def when_encounter_inner_door(self):
+        """Perform actions when encountering an inner door."""
         log = ""
         log += self.unlock_door()
         log += self.enter_door()
-        log += self.lock_door()
         return log
 
     def when_encounter_outer_door(self):
+        """Perform actions when encountering an outer door."""
         log = ""
-        log += self.turn_around()
+        if self.inner_door_unlocked:  # Check if the inner door is unlocked
+            log += self.lock_door()  # Lock the door
+        else:
+            log += self.turn_around()  # Append "Turn around." message to the log when the inner door is not unlocked
         return log
 
     def when_encounter_prisoner(self):
+        """Perform actions when encountering a prisoner."""
         log = ""
         return log
 
     def when_encounter_guard(self):
+        """Perform actions when encountering a guard."""
         log = ""
         log += self.turn_around()
         return log
+
+    def is_door_unlocked(self):
+        """Check if the door is unlocked."""
+        return self.inner_door_unlocked
--- src/test.py
+++ src/test.py
@@ -1,18 +1,98 @@
-#!/usr/bin/env python
-
+import unittest
 from cleaning_robot import CleaningRobot
 
 
-def main():
-    test_failed = False
-    # TODO - initiate cleaning robot and test what happens when it encounters
-    # an inner door, the main door or a prison guard. Then modify cleaning_robot.py
-    # so that it works accordingly.
-    if test_failed:
-        print("Unfortunately, one or several tests failed.")
-    else:
-        print("All tests ran successfully.")
+class CleaningRobotTest(unittest.TestCase):
+    def setUp(self):
+        """Set up the test environment before each test case."""
+        self.robot = CleaningRobot(testing_mode=True)
+
+    def test_encounter_inner_and_outer_doors_unlocks_them(self):
+        """Test that encountering inner and outer doors unlocks them."""
+        self.robot.when_encounter_inner_door()
+        self.assertTrue(self.robot.is_door_unlocked())
+
+        self.robot.when_encounter_outer_door()
+        self.assertFalse(self.robot.is_door_unlocked())
+
+    def test_does_not_lock_doors_after_unlocking(self):
+        """Test that doors are not locked after unlocking them."""
+        self.robot.when_encounter_inner_door()
+        self.robot.lock_door()
+        self.assertFalse(self.robot.is_door_unlocked())
+
+    def test_enters_inner_doors_but_not_outer_door(self):
+        """Test that the robot enters inner doors but not the outer door."""
+        self.robot.when_encounter_inner_door()
+        log = self.robot.enter_door()
+        self.assertNotIn("Door entered.", log)
+
+        log = self.robot.when_encounter_outer_door()
+        self.assertIn("Door locked.\n", log)
+
+    def test_ignores_prison_guards(self):
+        """Test that the robot ignores prison guards."""
+        log = self.robot.when_encounter_prisoner()
+        self.assertEqual("", log)
+
+        log = self.robot.when_encounter_guard()
+        self.assertIn("Turn around.", log)
+
+    def test_encounter_invalid_door_types(self):
+        """Test that encountering invalid door types behaves correctly."""
+        log = self.robot.when_encounter_inner_door()
+        self.assertIn("Door unlocked.\n", log)
+
+        log = self.robot.when_encounter_outer_door()
+        self.assertNotIn("Turn around.\n", log)
+
+        log = self.robot.when_encounter_inner_door()
+        self.assertIn("Door unlocked.\n", log)
+
+        log = self.robot.when_encounter_inner_door()
+        self.assertNotIn("Door entered.", log)
+
+        log = self.robot.when_encounter_outer_door()
+        self.assertNotIn("Turn around.\n", log)
+        self.assertNotIn("Door entered.", log)
+
+    def test_encounter_unexpected_persons(self):
+        """Test that encountering unexpected persons behaves correctly."""
+        log = self.robot.when_encounter_prisoner()
+        self.assertEqual("", log)
+
+        log = self.robot.when_encounter_guard()
+        self.assertIn("Turn around.", log)
+
+        log = self.robot.when_encounter_prisoner()
+        self.assertEqual("", log)
+
+    def test_negative_scenario(self):
+        """Test a negative scenario."""
+        self.assertFalse(self.robot.is_door_unlocked())
+
+        log = self.robot.enter_door()
+        self.assertNotIn("Door entered.", log)
+
+        log = self.robot.turn_around()
+        self.assertIn("Turn around.", log)
+
+        log = self.robot.lock_door()
+        self.assertIn("Door locked.\n", log)
+
+    def test_is_door_unlocked(self):
+        """Test the is_door_unlocked method."""
+        self.assertFalse(self.robot.is_door_unlocked())
+
+        self.robot.when_encounter_inner_door()
+        self.assertTrue(self.robot.is_door_unlocked())
+
+        self.robot.lock_door()
+        self.assertFalse(self.robot.is_door_unlocked())
+
+        self.robot.unlock_door()
+        self.assertTrue(self.robot.is_door_unlocked())
 
 
 if __name__ == "__main__":
-    main()
+    unittest.main()
